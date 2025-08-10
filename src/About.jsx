dbf64e57c7da431b0a94d4cbd41c65a3935328a8
Steg1118 <127258525+Steg1118@users.noreply.github.com>
2025-08-09 23:10:50 -0700

    dd

---
+++ src/About.jsx
@@ -0,0 +1,70 @@
+
+import './App.css';
+
+export default function Home() {
+  return (
+  
+    <div className="AboutMe">
+      <section>
+        <h1>About Me</h1>
+        <p>
+          Hello! My name is Sean. I’m a junior undergraduate student passionate about building games, learning full-stack web development, drawing, and exploring new creative projects. When I’m not coding, I enjoy traveling, gaming, and spending time with friends, family, and pets. My goal is to create engaging and interactive experiences through programming that bring joy to others, and to build a career doing what I love.
+        </p>
+      </section>
+
+      <section>
+        <h2>Me</h2>
+        <div>
+          <img src="images/seaninHK.jpg" alt="Image" className="gallery-image" />
+          <img src="images/mochasleep.jpg" alt="Image 2" className="gallery-image" />
+          <img src="images/headshot.jpg" alt="Image 3" className="gallery-image" />
+
+        </div>
+      </section>
+      <div className="spacer">
+        <section>
+        <h2 style={{ fontSize: '1.5rem' }}>Experience</h2>
+        <h2>Learning Assistant at University of California, Merced</h2>
+        <p><em>August 2024 – Present | Merced, CA</em></p>
+        <p>
+          I assist engineering students by teaching Introductory Calculus, Calculus 1, and Calculus 2, helping them build a strong foundation in mathematics.
+        </p>
+      </section>
+      
+      </div>
+      <section>
+        <h1>Learn More About Me</h1> 
+        <p>
+          My Top 9 Video Games:
+        </p>
+        <div>
+          <img src="images/topgames/portal2-1638924084230-522655275.jpg" alt="Image" className="gallery-image" />
+          <img src="images/topgames/elden-ring-button-03-1623460560664-3999650182.jpg" alt="Image 2" className="gallery-image" />
+          <img src="images/topgames/QKJRzanGk86ezpx2pk5QqQaduoXGJV3u8vHIejSav4MYiHA3F7zNgxSOF9bJmt2T-814084814.jpg" alt="Image 3" className="gallery-image" />
+        </div>
+        <div>
+          <img src="images/topgames/image-2140392718-1.jpg" alt="Image" className="gallery-image" />
+          <img src="images/topgames/fallout-new-vegas-500540987.png" alt="Image 2" className="gallery-image" />
+          <img src="images/topgames/sonic-adventure-2---button-1547850392122-3838835479.jpg" alt="Image 3" className="gallery-image" />
+        </div>
+        <div>
+          <img src="images/topgames/Eyd39JI06dtAxZReqdhJHXN5-3633685964.jpg" alt="Image" className="gallery-image" />
+          <img src="images/topgames/283094-minecraft-xbox-360-edition-xbox-360-front-cover-2807904834.png" alt="Image 2" className="gallery-image" />
+          <img src="images/topgames/1006146_front-1885294038.jpg" alt="Image 3" className="gallery-image" />
+        </div>
+        <p>
+          Check out my Steam Profile:
+        </p>
+        <a href="https://steamcommunity.com/id/steg1118/" target="_blank" rel="noopener noreferrer">
+           <img src={"images/steam-logo-steam-icon-transparent-free-png-3595373590.png"} className="logolink" alt="My Steam Profile" style={{width: '100px', borderRadius: '10px'}} />
+        </a>
+
+
+      </section>
+
+
+    </div>
+
+
+  );
+}
---
+++ src/Home.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  const [fade, setFade] = useState(true);
+  const messages = ["Hello","你好","Hola","こんにちは","Привет","مرحبا","नमस्ते"];
+
+  useEffect(() => {
+      const interval = setInterval(() => {
+        setFade(false);
+
+        setTimeout(() => {
+          setCount(prev => (prev + 1) % messages.length);
+          setFade(true);
+        }, 500);
+      }, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <>
+      <div>
+        <link rel="icon" href="images/tails.png" type="image/x-icon" />
+        <h2 className={"nametitle"}>Steg1118</h2>
+        <a href="https://github.com/Steg1118" target="_blank">
+          <img src={"images/snichead.png"} className="logo" alt="sonic logo" />
+        </a>
+      </div>
+      <h1 className={"header " + (fade ? "fade-in" : "fade-out")}>{messages[count]}</h1>
+        <h2>I'm Sean, Welcome to my website.</h2>
+      <div className="card">  
+        <p>
+          I create games, websites, art and software.
+        </p>
+      </div>
+      <p className="Introduction">
+        Feel free to explore my projects and learn more about me.
+      </p>
+      <a href="https://github.com/Steg1118" target="_blank">
+          <img src={"images/GitHub-Simbolo-1555934127.png"} className="logolink" alt="githublogo" style={{ width: "100px", height: "50px" }} />
+      </a>
+      <a href="https://github.com/Steg1118" target="_blank">
+          <img src={"images/Linkedin-Logo-3802517563.png"} className="logolink" alt="LinkedInlogo" style={{ width: "100px", height: "50px" }} />
+      </a>
+    </>
+  )
+}
+
