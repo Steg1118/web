6cafe6131067f4307bc8338625b1d69dd9edf9a4
Steg1118 <127258525+Steg1118@users.noreply.github.com>
2025-08-09 22:57:30 -0700

    Deploy React site

--- testing/script.js
+++
@@ -1,9 +0,0 @@
-console.log("penit effe");
-var number = 5;
-var string = 'Hello Penit';
-
-
-document.getElementById("but").addEventListener("click", function(){
-    console.log("Hello Penit");
-    document.getElementById("box").innerHTML = "<p>Hello Penit</p>";
-});
