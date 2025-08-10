3cca6efa81b659d28ab3dcbbc8b4494a36d36b2c
Steg1118 <127258525+Steg1118@users.noreply.github.com>
2025-08-09 23:14:52 -0700

    s

---
+++ src/navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import './App.css'
+import About from './About.jsx';
+import Home from './Home.jsx';
+import Project from './Projects.jsx';
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div>
+        <Link to="/" className='navLink'>Home</Link>
+        <Link to="/projects" className='navLink'>Projects</Link>
+        <Link to="/about" className='navLink'>About Me</Link>
+      </div>
+       <div className="nav-logo-wrapper">
+        <p>STEG</p>
+      </div>
+    </nav>
+  );
+}
